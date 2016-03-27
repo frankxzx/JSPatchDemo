e7ee6938887cb31955133a7662758c403c43d663
frank.xu <frank.xu@touch-spring.com>
2016-03-27 14:25:49 +0800

    Initial Commit

---
+++ JSPatchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSPatchDemo
+//
+//  Created by Xuzixiang on 16/3/27.
+//  Copyright © 2016年 TouchSpring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSPatchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSPatchDemo
+//
+//  Created by Xuzixiang on 16/3/27.
+//  Copyright © 2016年 TouchSpring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
